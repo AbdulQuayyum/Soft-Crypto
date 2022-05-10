30ebcf6852eadb41581e93c57f5c1f09fc2d4b7d
Abdul Quayyum <alaoabdulquayyumm@gmail.com>
2022-05-10 14:20:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const App = () => (
 <div className='footer'>
 <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
 Copyright © 2021
-<Link to='/'>Cryptoverse Inc.</Link> <br />
+<Link to='/'>Soft-Crypto Inc.</Link> <br />
 All Rights Reserved.
 </Typography.Title>
 <Space>
