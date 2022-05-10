e57add20cf0ac0cd4bce24414f7590f5ced492f4
Abdul Quayyum <alaoabdulquayyumm@gmail.com>
2022-05-10 14:12:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ News
 } from './components'
 import './App.css'
 
-const App = () => {
-return (
+const App = () => (
 <div className='app'>
 <div className='navbar'>
 <Navbar />
@@ -40,10 +39,20 @@ return (
 </Switch>
 </div>
 </Layout>
+<div className='footer'>
+<Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+Copyright © 2021
+<Link to='/'>Cryptoverse Inc.</Link> <br />
+All Rights Reserved.
+</Typography.Title>
+<Space>
+<Link to='/'>Home</Link>
+<Link to='/exchanges'>Exchanges</Link>
+<Link to='/news'>News</Link>
+</Space>
 </div>
-<div className='footer'></div>
+</div>
 </div>
 )
-}
 
 export default App
